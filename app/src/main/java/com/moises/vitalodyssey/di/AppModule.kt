13989160fece5e5bdccf6dfadb5b79ff01353852
Moises Sanchez <expresiomstudio@gmail.com>
2026-02-27 22:10:00 +0100

    feat: arquitectura base compilada y lista para la capa de presentación

--- app/src/main/java/com/moises/vitalodyssey/di/AppModule.kt
+++ app/src/main/java/com/moises/vitalodyssey/di/AppModule.kt
@@ -4,16 +4,18 @@ import androidx.room.Room
 import com.moises.vitalodyssey.data.local.AppDatabase
 import com.moises.vitalodyssey.data.local.UserPreferencesManager
 import com.moises.vitalodyssey.domain.usecase.CalculateBattleTurnUseCase
+import com.moises.vitalodyssey.domain.usecase.CalculateBossStatsUseCase
 import com.moises.vitalodyssey.domain.usecase.CalculatePlayerStatsUseCase
+import com.moises.vitalodyssey.domain.usecase.ProcessBattleResultUseCase
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val appModule = module {
 
-    // 1. DataStore (Preferencias del Usuario) - Usamos 'single' para que solo exista uno en toda la app
+    // 1. DataStore (Preferencias del Usuario) - 'single' para que solo exista uno
     single { UserPreferencesManager(androidContext()) }
 
-    // 2. Base de Datos Local (Room) - También debe ser 'single'
+    // 2. Base de Datos Local (Room)
     single {
         Room.databaseBuilder(
             androidContext(),
@@ -25,9 +27,11 @@ val appModule = module {
     // 3. DAOs (Para acceder a las tablas)
     single { get<AppDatabase>().habitDao() }
 
-    // 4. Casos de Uso (Lógica de negocio) - Usamos 'factory' para crear una instancia nueva cada vez que se necesiten
+    // 4. Casos de Uso (Lógica de negocio RPG) - 'factory' crea una instancia nueva cada vez
     factory { CalculatePlayerStatsUseCase() }
     factory { CalculateBattleTurnUseCase() }
     factory { CalculateBossStatsUseCase() }
+
+    // El orquestador necesita el CalculatePlayerStatsUseCase, Koin lo inyecta solo usando 'get()'
     factory { ProcessBattleResultUseCase(get()) }
 }
